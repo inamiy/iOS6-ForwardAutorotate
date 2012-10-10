c2a399b36a8c0db545ea7107d48b196111df8b59
Yasuhiro Inami <inamiy@gmail.com>
2012-10-10 16:02:41 +0900

    Add iOS6ForwardAutorotate.h

---
+++ iOS6ForwardAutorotate.h
@@ -0,0 +1,11 @@
+//
+//  iOS6ForwardAutorotate.h
+//  iOS6ForwardAutorotate
+//
+//  Created by Inami Yasuhiro on 12/10/10.
+//  Copyright (c) 2012年 Inami Yasuhiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UINavigationController+iOS6ForwardAutorotate.h"
+#import "UITabBarController+iOS6ForwardAutorotate.h"
